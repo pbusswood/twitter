7ad120e095c6a4314a76a15c6fb1d6bf3c8f6649
Parker Busswood <parkerbusswood@gmail.com>
2013-07-26 15:05:34 +0000

    Fix for login form and current user check

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :check_user_login
+
   def index
     @tweets = Tweet.all
   end
@@ -13,7 +15,7 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.create(tweet_params)
-    @tweet.user = User.first
+    @tweet.user = @current_user
     @tweet.save
     redirect_to tweets_path
   end
@@ -25,5 +27,9 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+
+  def check_user_login
+    @current_user = User.find(session[:user_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :check_user_login
+  before_action :check_user_login, :only => [:show]
 
   def new
     @user = User.new
